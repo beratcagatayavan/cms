eb630af6215b8237230340c0be476434468c45f4
beratcagatayavan <avanberatcagatay@gmail.com>
2022-03-11 13:54:16 +0300

    navbar düzenlemesi yapıldı

--- panel/application/views/dashboard_v.php
+++ panel/application/views/dashboard_v.php
@@ -34,10 +34,6 @@
     </main>
     <!--========== END app main -->
 
-    <!-- SIDE PANEL -->
-    <?php $this->load->view("includes/right_aside"); ?>
-    <!-- /#side-panel -->
-
 <?php $this->load->view("includes/include_script"); ?>
 
 </body>
