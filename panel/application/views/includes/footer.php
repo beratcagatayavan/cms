73c0b2ace0ebdd0221743b4c6cb5c83954830769
beratcagatayavan <avanberatcagatay@gmail.com>
2022-03-11 11:44:00 +0300

    dashboar_v klasorü oluşturuldu boş content sayfası hazırlandı

---
+++ panel/application/views/includes/footer.php
@@ -0,0 +1,7 @@
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+        <div class="clearfix">
+            <div class="copyright" style="text-align: center !important; float: inherit !important;">Berat Çağatay AVAN CMS 2022 &copy;</div>
+        </div>
+    </footer>
+</div>
