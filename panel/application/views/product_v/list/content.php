baabfa0cc70a63b7815cbdc5384d92825d75e157
beratcagatayavan <avanberatcagatay@gmail.com>
2022-03-12 14:35:37 +0300

    Ürün listesi veri tabanından getirildi ve dinamikleştirildi. Product Model Kodlandı

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -8,12 +8,13 @@
     <div class="col-md-12">
         <div class="widget p-lg">
 
-            <div class="alert alert-info text-center">
-                <h5 class="alert-title"><b>Ürün Bulunamadı</b></h5>
-                <p>Ürün Eklemek İçin <b>Yeni Ürün Ekle</b> Butonuna Basınız</p>
-            </div>
-
-            <table class="table table-hover table-striped">
+            <?php if(empty($items)) { ?>
+                <div class="alert alert-info text-center">
+                    <h5 class="alert-title"><b>Ürün Bulunamadı</b></h5>
+                    <p>Ürün Eklemek İçin <b>Yeni Ürün Ekle</b> Butonuna Basınız</p>
+                </div>
+            <?php } else { ?>
+                <table class="table table-hover table-striped">
                 <thead>
                     <th>#id</th>
                     <th>Url</th>
@@ -23,47 +24,28 @@
                     <th></th>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>#1</td>
-                        <td>monitor-askisi</td>
-                        <td>Monitör Askısı</td>
-                        <td>360 Derece Kullanılabilen Monitör Askısıdır</td>
-                        <td>
-                            <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
-                        </td>
-                        <td>
-                            <a href="#" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a>
-                            <a href="#" class="btn btn-xs btn-warning"><i class="fa fa-pencil"></i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>#2</td>
-                        <td>monitor-askisi2</td>
-                        <td>Monitör Askısı2</td>
-                        <td>360 Derece Kullanılabilen Monitör Askısıdır</td>
-                        <td>
-                            <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
-                        </td>
-                        <td>
-                            <a href="#" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a>
-                            <a href="#" class="btn btn-xs btn-warning"><i class="fa fa-pencil"></i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>#3</td>
-                        <td>monitor-askisi3</td>
-                        <td>Monitör Askısı3</td>
-                        <td>360 Derece Kullanılabilen Monitör Askısıdır</td>
-                        <td>
-                            <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
-                        </td>
-                        <td>
-                            <a href="#" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a>
-                            <a href="#" class="btn btn-xs btn-warning"><i class="fa fa-pencil"></i></a>
-                        </td>
-                    </tr>
+                    <?php foreach ($items as $item) { ?>
+                        <tr>
+                            <td>#<?php echo $item->id; ?></td>
+                            <td><?php echo $item->url; ?></td>
+                            <td><?php echo $item->title; ?></td>
+                            <td><?php echo $item->description; ?></td>
+                            <td>
+                                <input
+                                    type="checkbox"
+                                    data-switchery data-color="#10c469"
+                                    <?php echo ($item->isActive) ? "checked" : ""; ?>
+                                />
+                            </td>
+                            <td>
+                                <a href="#" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a>
+                                <a href="#" class="btn btn-xs btn-warning"><i class="fa fa-pencil"></i></a>
+                            </td>
+                        </tr>
+                    <?php } ?>
                 </tbody>
             </table>
+            <?php } ?>
         </div>
     </div>
 </div>
