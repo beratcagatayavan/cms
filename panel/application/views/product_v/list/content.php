38d9bd28da5e0fa6ffb1163d0533ab21b2a939b0
beratcagatayavan <avanberatcagatay@gmail.com>
2022-03-12 10:55:39 +0300

    urunler modulünün ürün listesi için html şablonu oluşturuldu.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,54 @@
-Ürün listesi content..
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Ürün Listesi</h4>
+    </div>
+    <div class="col-md-12">
+        <div class="widget p-lg">
+            <table class="table table-hover table-striped">
+                <thead>
+                    <th>#id</th>
+                    <th>Url</th>
+                    <th>Ürün Adı</th>
+                    <th>Açıklama</th>
+                    <th>Durumu</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 Derece Kullanılabilen Monitör Askısıdır</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a>
+                            <a href="#" class="btn btn-xs btn-warning"><i class="fa fa-pencil"></i></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#2</td>
+                        <td>monitor-askisi2</td>
+                        <td>Monitör Askısı2</td>
+                        <td>360 Derece Kullanılabilen Monitör Askısıdır</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a>
+                            <a href="#" class="btn btn-xs btn-warning"><i class="fa fa-pencil"></i></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#3</td>
+                        <td>monitor-askisi3</td>
+                        <td>Monitör Askısı3</td>
+                        <td>360 Derece Kullanılabilen Monitör Askısıdır</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a>
+                            <a href="#" class="btn btn-xs btn-warning"><i class="fa fa-pencil"></i></a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
