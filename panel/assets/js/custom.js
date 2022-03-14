13be3e7eebebebad7453a108be33d55e740fb51b
beratcagatayavan <avanberatcagatay@gmail.com>
2022-03-14 16:18:27 +0300

    isActiveSetter işlemi hem controller'da hem de custom.js üzerinde gerçekleştirildi

--- panel/assets/js/custom.js
+++ panel/assets/js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $(".remove-btn").click(function (e) {
+    $(".remove-btn").click(function () {
 
         var $data_url = $(this).data("url");
 
@@ -15,11 +15,21 @@ $(document).ready(function () {
             confirmButtonText: 'Evet'
         }).then(function (result)  {
             if (result.value) {
-
                window.location.href = $data_url;
-
             }
         });
+    })
+
+    $(".isActive").change(function (){
+
+        var $data = $(this).prop("checked");
+        var $data_url = $(this).data("url");
+
+        if (typeof $data !== "undefined" && typeof $data_url !== "undefined"){
+
+            $.post($data_url, { data : $data }, function (response) {});
+
+        }
 
     })
 
